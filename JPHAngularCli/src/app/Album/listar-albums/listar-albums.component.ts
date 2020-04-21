a8309b946657638c09a560f3051eb33d472465d8
Tauã Brandão <brandao.taua@gmail.com>
2020-04-21 18:01:27 -0300

    Consumindo API do JSON Place Holder para exibir os albums na tela

--- JPHAngularCli/src/app/Album/listar-albums/listar-albums.component.ts
+++ JPHAngularCli/src/app/Album/listar-albums/listar-albums.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlbumService } from 'src/app/album.service';
+import { i18nMetaToDocStmt } from '@angular/compiler/src/render3/view/i18n/meta';
 
 @Component({
   selector: 'app-listar-albums',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarAlbumsComponent implements OnInit {
 
-  constructor() { }
+  // array onde será armazenado a resposta da API
+  albums: Array<any>;
+
+  // direto no construtor, uma instancia de AlbumService
+  constructor(private service:AlbumService) { }
 
   ngOnInit(): void {
+    // assim que o componente for inteiramente carregado, esse método será chamado
+    this.listar();
+  }
+
+  listar(){
+    // utiliza o servico de AlbumService instanciado para chamar o metodo
+    // listarAlbums e utiliza uma arrow function para armazenar o resultado no array albums
+    this.service.listarAlbums().subscribe(dados => this.albums = dados);
   }
 
 }
--- JPHAngularCli/src/app/album.service.ts
+++ JPHAngularCli/src/app/album.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumService {
 
-  constructor() { }
+  //url onde a requisição será feita
+  albunsUrl = 'https://jsonplaceholder.typicode.com/albums';
+
+  // HttpClient passado direto no construtor
+  constructor(private service:HttpClient) { }
+
+  listarAlbums(){
+    // utiliza o servico passado no construtor, envia um get
+    // que espera um array de qualquer coisa
+    return this.service.get<any[]>(`${this.albunsUrl}`);
+  }
+
 }
