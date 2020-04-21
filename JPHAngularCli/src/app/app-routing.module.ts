38410e65c6d3953cca079b2d3a3656a19a4bf47e
Tauã Brandão <brandao.taua@gmail.com>
2020-04-21 17:27:58 -0300

    Configuração de rota para listar comments

--- JPHAngularCli/src/app/app-routing.module.ts
+++ JPHAngularCli/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListarAlbumsComponent } from './Album/listar-albums/listar-albums.component';
+import { ListarCommentsComponent } from './Comment/listar-comments/listar-comments.component';
 
 
 //declaração das rotas.
 //todas as rotas devem ser declaradas aqui.
 const routes: Routes = [
   {path:'getAlbums', component:ListarAlbumsComponent},
+  {path:'getComments', component:ListarCommentsComponent},
 ];
 
 @NgModule({
